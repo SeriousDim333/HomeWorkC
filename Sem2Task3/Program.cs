fd93ac5465dd1795712f53e0014f2ed0c3b81ad0
Dmitriy <seriousdim@mail.ru>
2022-12-23 18:07:45 +0300

    изменил решение третьей задачи

--- Sem2Task3/Program.cs
+++ Sem2Task3/Program.cs
@@ -3,7 +3,7 @@
 //78 -> третьей цифры нет
 //32679 -> 6
 
-int numberOfDigits(int number)
+/*int numberOfDigits(int number)
 {
     int i = 10;
     int count = 1;
@@ -17,21 +17,49 @@ int numberOfDigits(int number)
 
 double result(int number)
 {
-    int count = numberOfDigits(number);
-    double threeNum = number % ((Math.Pow(10, count)) / 1000);
-    double secondNum = number % ((Math.Pow(10, count)) / 100);
-    double res = (secondNum - threeNum) / ((Math.Pow(10, count)) / 1000);
+    //int count = numberOfDigits(number);
+    double threeNum = number % ((Math.Pow(10, numberOfDigits(number))) / 1000);
+    double secondNum = number % ((Math.Pow(10, numberOfDigits(number))) / 100);
+    double res = (secondNum - threeNum) / ((Math.Pow(10, numberOfDigits(number))) / 1000);
     return res;
 }
 
 int number = int.Parse(Console.ReadLine());
 
-if(number>100)
+if(number>99)
 {
     double res = result(number);
-    Console.WriteLine(res);
+    Console.WriteLine(result(number));
 }
 else
 {
     Console.WriteLine("третьей цифры нет");
+}*/
+
+int getNumber(string messege)
+{
+    Console.WriteLine(messege);
+    int number = int.Parse(Console.ReadLine());
+    return number;
 }
+int threeDidget( int number)
+{
+    while (number / 1000 > 0)
+    {
+        number = number / 10;
+    }
+    int threeDid = number % 10;
+    return threeDid;
+}
+
+int number = getNumber("введите число");
+if(number/100 == 0)
+{
+    Console.WriteLine("нету третьей цифры");
+}
+else
+{
+int threeDid = threeDidget(number);
+Console.WriteLine(threeDid);
+}
+
