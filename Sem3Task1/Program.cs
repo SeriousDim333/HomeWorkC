3cb1cb215488725ed66dc282962832fcaa593be5
Dmitriy <seriousdim@mail.ru>
2022-12-24 13:23:12 +0300

    добавил решение первой задачи третьего семинара

---
+++ Sem3Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+
+int number(string messege)
+{
+    Console.WriteLine(messege);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void polindrom(int number)
+{
+    if (number / 10000 == number % 10 && (number / 1000) % 10 == (number / 10) % 10)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+
+int num = number("введите пятизначное число");
+polindrom(num);
