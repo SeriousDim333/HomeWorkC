f5e5203a8d9352535ccf71bf0029034c4f473a26
Dmitriy <seriousdim@mail.ru>
2023-01-13 14:32:21 +0300

    добавил решение первой задачи четвертого семинара

---
+++ Sem4Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+int number(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int exponentional(int num,int num2)
+{
+int expo = num;
+for (int i = 1; i < num2; i++)
+{
+    expo = expo*num;
+}
+return expo;
+}
+
+int num = number("введите число");
+int num2 = number("введите степень");
+Console.WriteLine($"{num} в степени {num2} = {exponentional(num,num2)}");
