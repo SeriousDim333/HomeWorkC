895f3cc1743e3b408870819e39ac6e89458c7391
Dmitriy <seriousdim@mail.ru>
2023-01-13 14:48:29 +0300

    добавил решение второй задачи четвертого семинара

---
+++ Sem4Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+
+int number(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int summa(int num)
+{
+    int sum = 0;
+    while (num % 10 > 0 || num/10>0)
+    {
+        sum += num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+int num = number("введите число");
+Console.WriteLine($"сумма цифр числа {num} равна: {summa(num)}");
