6cb6874d0571165cce898369c1506ed99a43e383
Dmitriy <seriousdim@mail.ru>
2023-01-17 14:40:53 +0300

    добавил решение первой задачи пятого семинара

---
+++ Sem5Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+
+
+int[] generateArray(int length, int start, int end)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(start, end);
+    }
+    return array;
+}
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if(i<array.Length -1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int countEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]%2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] array = generateArray(5,100,1000);
+printArray(array);
+Console.WriteLine($"четных чисел {countEven(array)}");
