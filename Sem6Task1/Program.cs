e27495727bdedc48047fb5aa6280cd3ebd656beb
Dmitriy <seriousdim@mail.ru>
2023-01-20 17:18:40 +0300

    добавил решение первой задачи шестого семинара

---
+++ Sem6Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+int number(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int[] array(int length)
+{
+    int[] arr = new int[length];
+    return arr;
+}
+
+int[] fillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = number("введите число массива");
+    }
+    return array;
+}
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int positiveNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        count = array[i] > 0 ? count+1 : count;
+    }
+    return count;
+}
+
+int length = number("введите длину массива");
+int[] newArray = array(length);
+fillArray(newArray);
+printArray(newArray);
+Console.WriteLine($"количество положителюных чисел: {positiveNumber(newArray)}");
